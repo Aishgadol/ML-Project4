b08cc723e574df9404228cced0bd09c36ff6c0f9
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-03-06 15:45:02 +0200

    slight progress

--- ML-Project4/PCA.py
+++ ML-Project4/PCA.py
@@ -5,7 +5,12 @@ import matplotlib.pyplot as plt
 
 def PCA_train(data, k):
     means=np.mean(data,axis=0)
-
+    data=data-means
+    scatter_mat=np.dot(data.T,data)
+    eigenvalues, eigenvectors = np.linalg.eig(scatter_mat)
+    E=eigenvectors[:,np.argsort(eigenvalues)[-k:]].transpose()
+    y=np.matmul(E,data.transpose())
+    return y
 	# Download data to k dimensions
 
 def PCA_test(test, mu, E):
--- ML-Project4/experiments.py
+++ ML-Project4/experiments.py
@@ -3,25 +3,35 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import matplotlib.pyplot as plt
 
-data= np.random.randint(1,100,(30, 2))
-print(data)
-means=np.mean(data,axis=0)
-print(np.mean(data,axis=0))
-centerd_data=data-means
 def plotty(data,centered_data):
-    plt.figure(figsize=(5, 3))
+    plt.figure(figsize=(10, 8))
     plt.title('Random Dataset')
     plt.xlabel('Features')
     plt.ylabel('Values')
-    plt.axhline(0, color='black', linewidth=1.0)
-    plt.axvline(0, color='black', linewidth=1.0)
+    #plt.axhline(0, color='black', linewidth=1.0)
+    #plt.axvline(0, color='black', linewidth=1.0)
     plt.scatter(data[:, 0], data[:, 1], label='Samples', color='blue')
     plt.scatter(data[:, 0], data[:, 1], label='Original Data', color='red')
 
     # Plotting centered data in blue
     plt.scatter(centered_data[:, 0], centered_data[:, 1], label='Centered Data', color='blue')
     plt.legend()
-    plt.grid(False)
+    plt.grid(True)
     plt.show()
+data= np.random.randint(1,100,(30, 5))
+print(data)
+means=np.mean(data,axis=0)
+centerd_data=data-means
+print(centerd_data)
+print(np.mean(data,axis=0))
 print(np.mean(centerd_data,axis=0))
-plotty(data,centerd_data)
+scatter=np.dot(centerd_data.T,centerd_data)
+print(scatter.shape)
+eigenvalues,eigenvectors=np.linalg.eig(scatter)
+#print(eigenvectors)
+#print(np.argsort(eigenvalues))
+print(data.shape)
+E=eigenvectors[:,np.argsort(eigenvalues)[-2:]].T
+print(E.shape)
+y=np.matmul(E,centerd_data.T).T
+print(y)
