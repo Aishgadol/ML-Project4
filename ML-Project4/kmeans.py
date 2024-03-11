9609b73b876aa810ff5288845fb5b0e665f9d5fa
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-03-11 17:01:01 +0200

    working on clustering implementation

--- ML-Project4/kmeans.py
+++ ML-Project4/kmeans.py
@@ -22,7 +22,7 @@ class Kmeans:
 		centroids = np.zeros((self.n_clusters, X.shape[1]))
 		# Implement here
 		for i in range(self.n_clusters):
-			centroids[i]=np.mean(X[labels==k])
+			centroids[i]=np.mean(X[labels==i],axis=0)
 		return centroids
 
 	def compute_distance(self, X, centroids):
@@ -47,8 +47,8 @@ class Kmeans:
 			old_centroids = self.centroids
 			# For each point, calculate distance to all k clustes.
 			distances=self.compute_distance(X,old_centroids)
-			self.labels =	np.min(distances,axis=1) # Assign the labels with closest distance' cluster.
-			self.centroids = self.reassign_centroids(X, labels)# Update the centroids
+			self.labels =np.argmin(distances,axis=1) # Assign the labels with closest distance' cluster.
+			self.centroids = self.reassign_centroids(X, self.labels)# Update the centroids
 			if np.all(old_centroids == self.centroids):
 				break
 		self.error = self.compute_sse(X, self.labels, self.centroids)
@@ -58,8 +58,8 @@ class Kmeans:
 		return self.find_closest_cluster(distance)
 
 df = pd.read_csv('exams.csv').to_numpy()
-data=df[:,:2]
-labels=df[:,2]
+data=df[:,:-1]
+labels=df[:,-1]
 data_label0=data[labels==0]
 data_label1=data[labels==1]
 plt.figure(figsize=(8, 6))
@@ -69,5 +69,22 @@ plt.xlabel('Feature 1')
 plt.ylabel('Feature 2')
 plt.title('Scatter Plot of Data with Labels')
 plt.legend()
-plt.grid(True)
+plt.grid(False)
 plt.show()
+
+clust=Kmeans(n_clusters=2)
+clust.fit(data)
+labels = clust.labels
+centroids = clust.centroids
+for i in range(len(centroids)):
+    print(f'centroid {i} is at: {centroids[i]}')
+c0 = data[labels == 0]
+c1 = data[labels == 1]
+
+plt.scatter(c0[:,0], c0[:,1], c='green', label='cluster 1')
+plt.scatter(c1[:,0], c1[:,1], c='blue', label='cluster 2')
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=200, c='black', label='centroid')
+#plt.legend()
+
+plt.show()
+
