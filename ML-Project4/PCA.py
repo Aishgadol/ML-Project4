0c4fa771c723fa4d1360500bd61c9d0a006e211f
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-03-11 17:13:05 +0200

    working on clustering implementation

--- ML-Project4/PCA.py
+++ ML-Project4/PCA.py
@@ -20,3 +20,43 @@ def PCA_test(test, mu, E):
 def recover_PCA(data, mu, E):
     new_data=np.mul(np.linalg.inv(E),data.T)+mu
     return new_data
+
+x_train_new, mu, E = PCA_train(x_train_flatten, 81)
+x_test_new = PCA_test(x_test_flatten, mu, E)
+x_train_new_unflatten=x_train_new.reshape(len(x_train_new),9,9)
+x_test_new_unflatten=x_test_new.reshape(len(x_test_new),9,9)
+
+plt.subplot(131)
+plt.title("me b4 hw4")
+plt.imshow(x_train[5], cmap='gray')
+
+plt.subplot(132)
+plt.title("mw during hw4")
+plt.imshow(x_train_new_unflatten[5].astype(float), cmap='gray')
+
+plt.subplot(133)
+plt.title("me after hw4")
+plt.imshow(recover_PCA(x_train_new[5],mu,E).reshape(64,64).astype(float), cmap='gray')
+
+plt.show()
+
+x_train_flatten_zero_mean=x_train_flatten-np.mean(x_train_flatten,axis=0)
+scatter=np.dot(x_train_flatten_zero_mean.T,x_train_flatten_zero_mean)
+eigenvalues, eigenvectors = np.linalg.eig(scatter)
+
+def EIG_CDF(eig_list):
+	total_eig_sum=sum(eig_list)
+	sorted_eigenvalues = np.sort(eig_list)[::-1]
+	print(sorted_eigenvalues)
+	eigenvalues_cumsum = np.cumsum(sorted_eigenvalues)
+	eigenvalues_cumsum_normalized = eigenvalues_cumsum / eigenvalues_cumsum[-1]
+	#find index where cumsum>=0.95
+	amount = np.argmax(eigenvalues_cumsum_normalized >= 0.95) +1
+
+	plt.plot(np.arange(1, len(sorted_eigenvalues)+1), eigenvalues_cumsum_normalized)
+	plt.xlabel('Principal Component')
+	plt.ylabel('Cumulative Proportion of Variance')
+	plt.title(f'CDF of Eigenvalues - {amount} eigs preserves 95% of enetry')
+	plt.show()
+
+EIG_CDF(eigenvalues)
